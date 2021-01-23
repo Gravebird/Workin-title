9105678abd8ef6ab4d0d09929e953a8472f9da53
Unknown <Ty@gimpel.ca>
2021-01-22 17:17:17 -0800

    Inital commit. Added the core mazeGame.py file. Added the .gitignore as well as a base requirements.txt

---
+++ mazeGame.py
@@ -0,0 +1,7 @@
+#The primary core of the [WORKIN' TITLE]. The core game will be run through here.
+#Created through the collaboration of
+#Tiras Gimpel
+#Kit Ho (Leo) Li
+#Bryan Rainbow
+#Jennie Wu
+#Xavier El Chantiry
