8b86b99ae7313726b396d57bb192dc884da69ae2
LeoLi <kl160@my.bcit.ca>
2021-02-06 23:03:11 -0800

    Part 3C
    Add a check in the while loop to make sure the player get all 4 items

--- mazeGame.py
+++ mazeGame.py
@@ -50,19 +50,19 @@ def move_player(direction: str, Px: int, Py: int)-> tuple:
     return (Px, Py)
 
 
-while m1.is_exit((Px, Py)) == False:
+while (m1.is_exit((Px, Py)) == False) or (len(m1.player.backpack) < 4):
     input_direction = input("Please enter a direction (a/s/w/d): ")
     (Px, Py) = move_player(input_direction, Px, Py)
-    
-    m1.display()
-
+        
     if m1.is_item((Px, Py)) == True:
         # Put the itme into the backpack and update the map
         m1.player.backpack = m1.map[Px][Py]
         m1.map[Px][Py] = " "
 
-
-        
+    # For test only
+    print(m1.player.backpack)
+    m1.display() 
+    # For test only
 
 
 
