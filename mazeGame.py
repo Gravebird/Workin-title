c36ce08345475182d3defc8eb561a2c58e912c3a
LeoLi <kl160@my.bcit.ca>
2021-02-26 21:42:24 -0800

    minor updates and add more comments

--- mazeGame.py
+++ mazeGame.py
@@ -31,42 +31,53 @@ def move_player(direction: str, Px: int, Py: int)-> tuple:
     if input_direction == "w":
         if m1.can_move_to(Px - 1, Py) == True:
             Px -= 1
-            print(Px, Py) # new coordinate
+            print(f"\nPlayer location at row {Px}, col {Py}") # new coordinate
     # Move down (s)
     if input_direction == "s":
         if m1.can_move_to(Px + 1, Py) == True:
             Px += 1
-            print(Px, Py) # new coordinate
+            print(f"\nPlayer location at row {Px}, col {Py}") # new coordinate
     # Move left (a)
     if input_direction == "a":
         if m1.can_move_to(Px, Py -1) == True:
             Py -= 1
-            print(Px, Py) # new coordinate
+            print(f"\nPlayer location at row {Px}, col {Py}") # new coordinate
     # Move right (d)
     if input_direction == "d":
         if m1.can_move_to(Px, Py + 1) == True:
             Py += 1
-            print(Px, Py) # new coordinate
+            print(f"\nPlayer location at row {Px}, col {Py}") # new coordinate
     
     return (Px, Py)
 
 
+# Initial Screen Display (Start)
+(width, height) = (1000, 600)
+screen = pygame.display.set_mode((width, height))
+m1.display(screen) 
+m1.tile_print(0, 0, "player", screen)
+pygame.display.flip()
+
+
+# Game Loop (May update later for the input/controller method to read from pygame keypress)
 while (m1.is_exit((Px, Py)) == False) or (len(m1.player.backpack) < 4):
     input_direction = input("Please enter a direction (a/s/w/d): ")
     (Px, Py) = move_player(input_direction, Px, Py)
         
     if m1.is_item((Px, Py)) == True:
-        # Put the itme into the backpack and update the map
+        # Put the item into the backpack and update the map
         m1.player.backpack = m1.map[Px][Py]
         m1.map[Px][Py] = " "
 
-    
+
+    # Re-draw
     (width, height) = (1000, 600)
     screen =pygame.display.set_mode((width, height))
-    pygame.display.flip()
-    # For test only
-    print(m1.player.backpack)
     m1.display(screen) 
-    m1.tile_print(Py, Px,"player",screen)
+    m1.tile_print(Py, Px, "player", screen)
     pygame.display.flip()
-    # For test only
+
+    ##################################
+    print(f"Backpack contains: {m1.player.backpack}")
+    
+
